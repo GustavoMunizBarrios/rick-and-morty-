a38cea32255afcd6b480559040cda4ed15f5671b
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-04-25 07:49:47 -0500

    Converti las promesas a async await

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -3,29 +3,35 @@ const URL ='https://richandmortyapi.com/api/character'
 const axios = require('axios');
 
 //La función recibe solicitudes HTTP GET y se encarga de buscar un personaje por su identificador (ID) en la API
-const getCharById = (req, res) => { 
-    const {id} = req.params; // destructuring del objeto params para obtener el id
-
-    // Petición a la API a partir del ID que recibimos por params.
-    axios(`${URL}/${id}`) //Por default hace un get
-    .then(response => response.data)
-    //Devuelve un JSON con las propiedades status, name, species, origin, image, gender.
-    .then(({status, name, species, origin, image, gender}) => { //recibimos a data con destructuring
-        if(name) { //si tengo un nombre significa que tengo personaje (que existe) entonces:
-            const character = { // Crea un objeto con las propiedades del personaje.
-                id, 
-                name,
-                species,
-                origin,
-                image,
-                gender,
-                status
-            }
-            return res.status(200).json(character) // Regresa el character 
+const getCharById = async (req, res) => { 
+    try {
+        const {id} = req.params; // destructuring del objeto params para obtener el id
+        const {data} = await axios(`${URL}/${id}`)
+        // Petición a la API a partir del ID que recibimos por params.
+        //Devuelve un JSON con las propiedades status, name, species, origin, image, gender.
+         //recibimos a data con destructuring
+        if(!data.name) throw new Error(`Faltan datos del personaje deon ID: ${id}`) // Si yo lanzo un error lo redirijo a catch(error)
+        //si tengo un nombre significa que tengo personaje (que existe) entonces:
+        const character = { // Crea un objeto con las propiedades del personaje.
+            id:data.id, 
+            name:data.name,
+            species:data.species,
+            origin:data.origin,
+            image:data.image,
+            gender:data.gender,
+            status:data.status
         }
-        return res.status(404).send('Not found'); // Si no tengo un name, no tengo un personaje por lo que regresa 'Not found'
-    })
-    .catch(error => res.status(500).send(error.message))
+        return res.status(200).json(character) // Regresa el character 
+        
+            //return res.status(404).send('Not found'); // Si no tengo un name, no tengo un personaje por lo que regresa 'Not found'
+        
+
+    } catch (error){
+        return error.message.includes('ID')
+        ? res.status(404).send(error.message) 
+        : res.status(500).send(error.response.data.error) // si se busca un ID que no existe entra aqui y devuelve 'Character not found'
+    }
+
 }
 //Nota: utilizamos send cuando mandamos texto plano.
 //Nota: utilizamos json cuando tenemos que mandar objetos de JS.
