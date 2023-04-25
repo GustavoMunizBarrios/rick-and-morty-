e83a67a3190082627b0454ed84fd0e749cdaa6f8
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-04-25 00:08:19 -0500

    Agregue comentarios al archivo index.js de la carpeta routes

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,16 +1,18 @@
+//Importación de todos los controladores 
 const {login} = require('../controllers/login')
 const {getCharById} =require('../controllers/getCharById')
 const {postFav, deleteFav} = require('../controllers/handleFavorites')
 
 const router = require('express').Router();
 
-router.get('/character/:id', (req, res) => {
+//Creación de rutas para cada controlador
+//La ruta va a ejecutar el controlador (ej. getCharById) cuando hagan una petición a la ruta especificada (ej. '/character/id')
+
+router.get('/character/:id', (req, res) => { // ('ruta', (req, res) => {} )
     getCharById(req,res);
 })
 
-router.get('/login', (req,res) => {
-    login(req,res);
-})
+router.get('/login', login) // Esta es la forma corta de escribirlo 
 
 router.post('/fav', (req, res) => {
     postFav(req,res);
