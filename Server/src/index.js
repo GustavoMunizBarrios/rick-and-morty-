e386078d6213c529135660bf156fd182a7e08810
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-04-25 00:19:27 -0500

    Agregue comentarios al archivo index.js principal y complete lo que faltaba

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,17 +2,29 @@
 const express = require('express')
 const server = express()
 const PORT = 3001
-const router = require('routes/index')
+const router = require('./routes/index')
 
-server.use(express.json());
+// Middleware que ejecuta a express.json().
+server.use(express.json());  // La información que llega en formato json la pasa a objeto de JS, para que la pueda trabajar
 
+//---------------------- Middleware ----------------------------
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header(
         'Access-Control-Allow-Headers',
-        '');
-})
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST OPTIONS, PUT, DELETE'
+    );
+    next();
+});
+//-----------------------------------------------------------------------
+
+//Middleware que agrega el string "/rickandmorty" antes de cada una de las rutas
+server.use("/rickandmorty", router)
 
 server.listen(PORT, () => { //Crear el servidor en express en el puerto 3001
     console.log(`Server raised in port: ${PORT}`);
