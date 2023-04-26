6da2b7515e2c7d564e364faef56095fd591934be
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-04-25 22:01:52 -0500

    Actualice la función addFav en actions.js

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -1,9 +1,19 @@
 import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from './action-types';
+import axios from 'axios';
 
 //Esta función recibe un personaje por parámetro. Retorna un payload que es igual al personaje
 export const addFav = (character) => {
-    return { type: ADD_FAV, payload: character }
-};
+    const endpoint = 'http://localhost:3001/rickandmorty/fav'; //guarda la URL de nuestro servidor
+    return (dispatch) => { //retorna una función haciendo una petición al servidor
+       axios.post(endpoint, character) // (ruta, el objeto character) character lo recibe por body nuestro servidor.
+       .then(({ data }) => { // destructuring de data
+          return dispatch({
+             type: ADD_FAV,
+             payload: data,
+          });
+       });
+    };
+ };
 
 //Esta función recibe un id por parámetro. Retorna un payload que es igual al id
 export const removeFav = (id) => { 
