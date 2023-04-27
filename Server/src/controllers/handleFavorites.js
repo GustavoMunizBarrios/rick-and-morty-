f7f49328673df0b5f057288a38e6333a78a32474
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-04-26 23:17:12 -0500

    ralice una validación a la función postFav para que no me permitiera traer personajes repetidos a myfavorites, utilice try-catch para el manejo de errores

--- Server/src/controllers/handleFavorites.js
+++ Server/src/controllers/handleFavorites.js
@@ -1,11 +1,19 @@
 let myFavorites = []; //simulando una base de datos de mis favoritos
 
 const postFav = (req, res) => {
-    const character = req.body; //En la propiedad body estará el personaje que le vamos a enviar a myFavorites
+    try {
+        const character = req.body; //En la propiedad body estará el personaje que le vamos a enviar a myFavorites
+        const characterFound = myFavorites.find(fav => fav.id === character.id) //Busca en myFavorites un character que sea igual al que recibimos (es decri que este repetido)
+        
+        if(characterFound) throw Error('El personaje ya existe en favoritos')// Si encuentra un character dentro de myFavorites entonces tira error
 
-    myFavorites.push(character)
-
-    return res.status(200).json(myFavorites)
+        myFavorites.push(character) // si NO encuentra el character dentro de myFavorites entonces pushea a character
+        return res.status(200).json(myFavorites) //retorna como respuesta al array myFavorites
+        
+    } catch (error) {
+        return res.status(404).send(error.message);
+        
+    }
 }
 
 const deleteFav = (req, res) => {
