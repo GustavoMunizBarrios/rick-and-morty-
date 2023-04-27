da23b54797d215c90ff7dbaea76bb99ac011364d
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-04-26 20:39:29 -0500

    RemovCree el archivo index.test en la carpeta test y modifique el archivo index.js general

---
+++ Server/src/app.js
@@ -0,0 +1,32 @@
+//"morgan" registra información útil, como el método HTTP utilizado (GET, POST, etc.), la URL solicitada,
+//la hora en que se recibió la solicitud y el tiempo de respuesta, entre otros detalles.
+const morgan = require('morgan');
+
+// Middleware que ejecuta a express.json().
+server.use(express.json());  // La información que llega en formato json la pasa a objeto de JS, para que la pueda trabajar
+server.use(morgan('dev'));
+
+//---------------------- Middleware ----------------------------
+const express = require('express')
+const server = express()
+const router = require('./routes/index')
+
+
+//Le da permiso al front, para que pueda acceder a nuestras rutas
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST OPTIONS, PUT, DELETE'
+    );
+    next();
+});
+//-----------------------------------------------------------------------
+
+//Middleware que agrega el string "/rickandmorty" antes de cada una de las rutas
+server.use("/rickandmorty", router) // primer parámetro el path, segundo donde estan todas las rutas
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,34 +1,5 @@
-const express = require('express')
-const server = express()
+const server =require('./app')
 const PORT = 3001
-const router = require('./routes/index')
-//"morgan" registra información útil, como el método HTTP utilizado (GET, POST, etc.), la URL solicitada,
-//la hora en que se recibió la solicitud y el tiempo de respuesta, entre otros detalles.
-const morgan = require('morgan');
-
-// Middleware que ejecuta a express.json().
-server.use(express.json());  // La información que llega en formato json la pasa a objeto de JS, para que la pueda trabajar
-server.use(morgan('dev'));
-
-//---------------------- Middleware ----------------------------
-//Le da permiso al front, para que pueda acceder a nuestras rutas
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST OPTIONS, PUT, DELETE'
-    );
-    next();
-});
-//-----------------------------------------------------------------------
-
-//Middleware que agrega el string "/rickandmorty" antes de cada una de las rutas
-server.use("/rickandmorty", router) // primer parámetro el path, segundo donde estan todas las rutas
 
 server.listen(PORT, () => { //Crear el servidor en express en el puerto 3001
     console.log(`Server raised in port: ${PORT}`);
