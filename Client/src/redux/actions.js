ff77f38b71bd359df1b028d41a4800a67eec599a
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-04-26 22:47:13 -0500

    Reemplace las promesas por async/await en la función addFav y removeFav utilizando try-catch

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -4,28 +4,40 @@ import axios from 'axios';
 //Esta función recibe un personaje por parámetro. Retorna un payload que es igual al personaje
 export const addFav = (character) => {
     const endpoint = 'http://localhost:3001/rickandmorty/fav'; //guarda la URL de nuestro servidor
-    return (dispatch) => { //retorna una función haciendo una petición al servidor
-       axios.post(endpoint, character) // (ruta, el objeto character) character lo recibe por body nuestro servidor.
-       .then(({ data }) => { // destructuring de data
-          return dispatch({
-             type: ADD_FAV,
-             payload: data,
-          });
-       });
+    return async (dispatch) => { //retorna una función haciendo una petición al servidor
+      try {
+         const {data} = await axios.post(endpoint, character) // (ruta, el objeto character) character lo recibe por body nuestro servidor.
+
+         if(!data.length) throw Error('No se pudo agregar a favoritos') //(data es un array con los personajes) Si data esta vacio tira error
+
+         return dispatch({
+            type: ADD_FAV,
+            payload: data,
+        });
+      } catch (error) {
+         console.log(error.message);
+      }
+       
     };
  };
 
 //Esta función recibe un id por parámetro. Retorna un payload que es igual al id
 export const removeFav = (id) => {
-    const endpoint = 'http://localhost:3001/rickandmorty/fav/' + id;
-    return (dispatch) => {
-       axios.delete(endpoint)
-       .then(({ data }) => {
-          return dispatch({
-                type: REMOVE_FAV,
-                payload: data,
-            });
-       });
+    const endpoint = `http://localhost:3001/rickandmorty/fav/${id}`;
+    return async (dispatch) => {
+      try {
+         const {data} = await axios.delete(endpoint);
+
+         if(!data.length) throw Error('No hay favoritos') //(data es un array con los personajes) Si data esta vacio tira error
+
+         return dispatch({
+            type: REMOVE_FAV,
+            payload: data,
+         });
+      } catch (error) {
+         console.log(error.message);
+      }
+      
     };
  };
 
