ed583668da669b03f58b4c05d4071b2be1036152
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-05-04 22:14:14 -0500

    Actualice modulos de node e hice cambios en el orden en el que se mandan llamar los middlewares en app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,16 +1,16 @@
+
 //"morgan" registra información útil, como el método HTTP utilizado (GET, POST, etc.), la URL solicitada,
 //la hora en que se recibió la solicitud y el tiempo de respuesta, entre otros detalles.
 const morgan = require('morgan');
-
-// Middleware que ejecuta a express.json().
-server.use(express.json());  // La información que llega en formato json la pasa a objeto de JS, para que la pueda trabajar
-server.use(morgan('dev'));
 
 //---------------------- Middleware ----------------------------
 const express = require('express')
 const server = express()
 const router = require('./routes/index')
 
+// Middleware que ejecuta a express.json().
+server.use(express.json());  // La información que llega en formato json la pasa a objeto de JS, para que la pueda trabajar
+server.use(morgan('dev'));
 
 //Le da permiso al front, para que pueda acceder a nuestras rutas
 server.use((req, res, next) => {
@@ -30,3 +30,4 @@ server.use((req, res, next) => {
 
 //Middleware que agrega el string "/rickandmorty" antes de cada una de las rutas
 server.use("/rickandmorty", router) // primer parámetro el path, segundo donde estan todas las rutas
+module.exports = server;
--- Server/src/index.js
+++ Server/src/index.js
@@ -3,4 +3,15 @@ const PORT = 3001
 
 server.listen(PORT, () => { //Crear el servidor en express en el puerto 3001
     console.log(`Server raised in port: ${PORT}`);
-})
+});
+
+module.exports = server;
+/* const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => { 
+  console.log(`Servidor escuchando en el puerto ${PORT}`); 
+}); */
+
+//process.setMaxListeners(20);
