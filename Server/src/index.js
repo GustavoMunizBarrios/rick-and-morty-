d351719bdbd4172f0d3791ac9100abb0f3c48a40
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-05-12 12:18:32 -0500

    Agregue comentarios adicionales al archivo index.js de Server

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,11 @@
-const server =require('./app')
-const PORT = 3001
+/*  Este archivo configura y levanta un servidor Express en el puerto 3001 y 
+exporta ese servidor para que pueda ser utilizado en otros archivos.  */
 
-server.listen(PORT, () => { //Crear el servidor en express en el puerto 3001
+const server =require('./app')// Importa el módulo 'app' desde el archivo app.js y lo asigna a la variable server
+const PORT = 3001 //Define el número de puerto en el que se ejecutará el servidor
+
+server.listen(PORT, () => { //Inicia el servidor y lo pone en escucha en el puerto 3001, 
+  // cuando se recibe una solicitud en ese puerto, se ejecutará el console.log
     console.log(`Server raised in port: ${PORT}`);
 });
 
