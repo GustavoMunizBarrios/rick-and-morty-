c72062008323902d02b7b8bf0c221ce00724f564
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-05-19 10:46:28 -0500

    Agregue el archivo styles al componente Nav, modifique el background de App.js

--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import SearchBar from '../SearchBar/SearchBar';
 import { Link} from 'react-router-dom';
+import style from './Nav.module.css';
 
 const Nav = ({ onSearch,  setAccess }) => {
 
@@ -8,23 +9,26 @@ const Nav = ({ onSearch,  setAccess }) => {
     }
 
     return (
-        <nav>
-            <SearchBar onSearch={onSearch}/>
+        <div className={style.home}>
+            <nav>
+                <SearchBar onSearch={onSearch}/>
 
-            <button>
-                <Link to='/about' >ABOUT</Link>
-            </button>
+                <button>
+                    <Link to='/about' >ABOUT</Link>
+                </button>
 
-            <button>
-                <Link to='/home' >HOME</Link>
-            </button>
+                <button>
+                    <Link to='/home' >HOME</Link>
+                </button>
 
-            <button>
-                <Link to='/favorites' >Favorites</Link>
-            </button>
+                <button>
+                    <Link to='/favorites' >Favorites</Link>
+                </button>
 
-            <button onClick={handlelogOut}>Log Out</button>
-        </nav>
+                <button onClick={handlelogOut}>Log Out</button>
+            </nav>
+
+        </div>
     )
 }
 
