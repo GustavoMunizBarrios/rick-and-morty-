8cc2e13d082e272ebf170f798a5921ec31bf4851
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-05-29 15:51:26 -0500

    Configure estilos del input de searchBar y de su botón

--- Client/src/components/SearchBar/SearchBar.jsx
+++ Client/src/components/SearchBar/SearchBar.jsx
@@ -9,12 +9,12 @@ export default function SearchBar({ onSearch }) {
    }
 
    return (
-      <div>
+      <div className={style.form}>
          <div className={style.form__group}>
             <input type='search' onChange={handleChange} value={id} className={style.form__field} placeholder='search' name="search"/> {/* La propiedad value me permite tener el mismo valor que id */ }
             <label htmlFor="search" className={style.form__label} >Buscar</label>
          </div>
-         <button onClick={() =>{onSearch(id); setId('')}}>Agregar</button>
+         <button className={style.form__btn} onClick={() =>{onSearch(id); setId('')}}>Agregar</button>
       </div>
    );
 }
