04b4a1da45c77610968345bf2d0bfd47cc7647cf
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-05-29 20:09:37 -0500

    Se creo el archivo css de Card.js para poder alinear correctamente las cards, también se realizaron ajustes en el archivo de estilos de index para complementar el disseño

--- Client/src/components/Cards/Cards.jsx
+++ Client/src/components/Cards/Cards.jsx
@@ -1,8 +1,9 @@
 import Card from '../Card/Card';
+import style from './Cards.module.css'
 
 export default function Cards({ characters, onClose }) {
    return (
-      <div>
+      <div className={style.main}>
          {
             characters.map(({ id, name, status, species, gender, origin, image }) => {
                return(
