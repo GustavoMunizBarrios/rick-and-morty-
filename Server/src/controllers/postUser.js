7009de92c4cfb0f528aced1c65ff6b82f0530552
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-06-09 15:25:08 -0500

    Cree el cuerpo del código y agregue comentarios

---
+++ Server/src/controllers/postUser.js
@@ -0,0 +1,19 @@
+const {User} = require('../DB_connection') //importa la tabla User del archivo DB_connection.js
+
+module.exports = async (req, res)=>{
+    
+    try {
+        const {email, password} =req.body //extrae del body email y password
+    
+        if(!email || !password) return res.status(400).send("Faltan datos")
+        
+        //Busca un registro en la DB User que coincida con el email y password del req.body,
+        //si no lo encuentra lo crea con los valores especificados.
+        //Si crea el registro devuelve también un true dentro del objeto JSON
+        const user = await User.findOrCreate({where: {email, password}})
+        return res.json(user)
+
+    } catch (error) {
+        return res.status(500).json(error.message)        
+    }
+}
