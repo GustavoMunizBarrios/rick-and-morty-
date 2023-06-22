8ee25441ef4cccda01fcbecc71e1f7d06bf72c86
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-06-22 14:42:26 -0500

    Agregue comentarios

--- Server/src/controllers/deleteFav.js
+++ Server/src/controllers/deleteFav.js
@@ -3,8 +3,9 @@ const {Favorite} = require('../DB_connection')
 module.exports = async (req, res) => {
     try {
         const {id} = req.params;
+        //Destruye el registro de la DB que coincida con el id pasado por params
         await Favorite.destroy({where:{id}})
-
+        //Busca todos los favoritos de la tabla Favorite y los guarda en la const allFavorite
         const allFavorite = await Favorite.findAll();
         return res.json(allFavorite)
 
