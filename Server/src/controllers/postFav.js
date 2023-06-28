be27fc3dd7d02a4f88b07db351d730bd6462c8dd
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-06-27 20:40:42 -0500

    Cambios menores

--- Server/src/controllers/postFav.js
+++ Server/src/controllers/postFav.js
@@ -1,9 +1,9 @@
 const {Favorite} = require("../DB_connection")//importa la tabla/modelo Favorite del archivo DB_connection.js
 
 module.exports = async (req, res)=>{
-    const {name, origin, status, image, species, gender} = req.body
+    const {name, image, status, origin, species, gender} = req.body
 
-    if(!name || !status || !species || !gender || !origin  || !image) return res.status(401).send('Faltan datos')
+    if(!name || !image || !status || !origin || !species || !gender) return res.status(401).send('Faltan datos')
     try {
 
         //Busca un registro en la tabla Favorite que coincida con name, origin, status, image, species, gender del req.body,
