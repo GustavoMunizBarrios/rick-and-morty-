d1cc936fb6be801ecdd956625017b25e6fe95cab
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2024-01-10 22:44:03 -0600

    I made the About screen responsive

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -1,8 +1,17 @@
+import style from "./About.module.css"
+
 const About = () => {
     return(
-        <div>
-            <h1>Work in progress...</h1>
-            <p>Se esta cocinando esta parte...</p>
+        <div className={style.container}>
+            <div className={style.container_paragraph}>
+                <h1>ABOUT</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Quisque vitae semper nunc. Sed at purus in massa laoreet tincidunt.
+                        Nulla facilisi. Sed cursus, enim a aliquam aliquet, lorem turpis semper metus, 
+                        id feugiat orci sapien at risus. Donec auctor, velit in interdum cursus, neque 
+                        dui convallis est, id tincidunt nisi sapien at arcu.
+                </p>
+            </div>
         </div>
     )
 }
